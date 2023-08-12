8df709609a156fee4625045fd39274e79cfe6bb1
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-08-12 00:37:31 -0400

    Shift in register test

--- Chessboard-Nano/src/main.cpp
+++ Chessboard-Nano/src/main.cpp
@@ -1,105 +1,50 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-const uint8_t I2C_ADDRESS = 0x50;
+const uint8_t shiftInLoadInPin = 8;
+const uint8_t shiftInClockEnablePin = 7;
+const uint8_t shiftInClockPin = 6;
+const uint8_t shiftInDataInPin = 5;
 
-const uint8_t rowPins[8] = {2, 3, 4, 5, 6, 7, 8, 9};         // outputs
-const uint8_t colPins[8] = {10, 11, 12, A0, A1, A2, A3, A6}; // inputs
-
-#define bitSet64(value, bit) ((value) |= (1ULL << (bit)))
-#define bitClear64(value, bit) ((value) &= ~(1ULL << (bit)))
-#define bitWrite64(value, bit, bitvalue) (bitvalue ? bitSet64(value, bit) : bitClear64(value, bit))
-
-uint64_t lastBoard = 0;
-uint64_t board = 0;
-bool boardChanged = false;
-uint8_t registerAddr = 0;
-
-uint64_t scanBoard();
-void printBoard();
-void onWireReceiveEvent(int count);
-void onWireRequestEvent();
-
-// clang-format off
-union packed_uint64_t {
-  uint64_t number;
-  uint8_t bytes[8];
-} packedBoard;
-// clang-format on
+void setupShiftInRegister();
+uint8_t readShiftInRegister();
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 
-  for (uint8_t row : rowPins) {
-    pinMode(row, OUTPUT);
-    digitalWrite(row, HIGH);
-  }
-  for (uint8_t col : colPins) {
-    if (col == A6) {
-      pinMode(col, INPUT); // pullup resistor in circuit
-    } else {
-      pinMode(col, INPUT_PULLUP);
-    }
-  }
-
-  Wire.begin(I2C_ADDRESS);
-  Serial.print("Joining I2C bus at address 0x");
-  Serial.println(I2C_ADDRESS, HEX);
-  Wire.onReceive(onWireReceiveEvent);
-  Wire.onRequest(onWireRequestEvent);
+  setupShiftInRegister();
 }
 
 void loop() {
-  board = scanBoard();
-  if (lastBoard != board) {
-    lastBoard = board;
-    noInterrupts();
-    packedBoard.number = board;
-    boardChanged = true;
-    interrupts();
-    Serial.println("Board state: ");
-    printBoard();
-  }
+  uint8_t data = readShiftInRegister();
+  Serial.print("Pin stats: ");
+  Serial.println(data, BIN);
+  delay(250);
 }
 
-uint64_t scanBoard() {
-  uint64_t tempBoard = 0;
-  for (uint8_t rowIndex = 0; rowIndex < 8; rowIndex++) {
-    digitalWrite(rowPins[rowIndex], LOW);
-    for (uint8_t colIndex = 0; colIndex < 8; colIndex++) {
-      if (colPins[colIndex] == A6 ? analogRead(A6) < 512 : (digitalRead(colPins[colIndex]) == LOW)) {
-        bitSet64(tempBoard, rowIndex * 8 + colIndex);
-      }
-    }
-    digitalWrite(rowPins[rowIndex], HIGH);
-  }
-  return tempBoard;
+void setupShiftInRegister() {
+  pinMode(shiftInLoadInPin, OUTPUT);
+  pinMode(shiftInClockEnablePin, OUTPUT);
+  pinMode(shiftInClockPin, OUTPUT);
+  pinMode(shiftInDataInPin, INPUT);
+
+  digitalWrite(shiftInLoadInPin, HIGH);
+  digitalWrite(shiftInClockEnablePin, HIGH);
+  digitalWrite(shiftInClockPin, LOW);
 }
 
-void printBoard() {
-  for (uint8_t i = 0; i < 64; i++) {
-    Serial.print(bitRead(board, i) == true ? 1 : 0);
-    if ((i + 1) % 8 == 0) {
-      Serial.println();
-    }
-  }
-}
+uint8_t readShiftInRegister() {
+  digitalWrite(shiftInLoadInPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(shiftInLoadInPin, HIGH);
+  delayMicroseconds(5);
 
-void onWireReceiveEvent(int count) {
-  registerAddr = Wire.read();
-}
+  digitalWrite(shiftInClockPin, HIGH);
+  digitalWrite(shiftInClockEnablePin, LOW);
+  uint8_t data = ~shiftIn(shiftInDataInPin, shiftInClockPin, LSBFIRST);
+  digitalWrite(shiftInClockEnablePin, HIGH);
 
-void onWireRequestEvent() {
-  const uint8_t boardOffset = 0x90;
-  if (registerAddr == boardOffset - 1) {
-    Wire.write((uint8_t)boardChanged);
-    boardChanged = false;
-  } else if (registerAddr >= boardOffset && registerAddr < boardOffset + 8) {
-    Wire.write(packedBoard.bytes[registerAddr - boardOffset]);
-  } else {
-    Wire.write((uint8_t)0);
-  }
-  registerAddr++;
+  return data;
 }
