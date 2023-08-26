7b7178c47713e73aea2416896835f9cb0bba4b4e
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-08-26 11:54:56 -0400

    Can rotate and flip board as needed

--- Chessboard-Nano/src/main.cpp
+++ Chessboard-Nano/src/main.cpp
@@ -22,6 +22,12 @@ const uint8_t shiftOutDataPin = 2;  // SER (14)
 #define bitClear64(value, bit) ((value) &= ~(1ULL << (bit)))
 #define bitWrite64(value, bit, bitvalue) (bitvalue ? bitSet64(value, bit) : bitClear64(value, bit))
 
+// 90, 180, 270 degree rotation
+// Comment out for no rotation
+#define ROTATE_BOARD 90
+// Comment out to not flip the board
+#define FLIP_BOARD
+
 uint64_t board = 0;
 uint64_t lastBoard = 0;
 bool boardChanged = false;
@@ -35,6 +41,9 @@ union packed_uint64_t {
 // clang-format on
 
 uint64_t scanBoard();
+#ifdef ROTATE_BOARD
+uint64_t rotateBoard90Degrees(uint64_t board);
+#endif
 void printBoard();
 void setupShiftInRegister();
 void setupShiftOutRegister();
@@ -45,11 +54,25 @@ void onWireRequestEvent();
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Initializing chess board");
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 
   setupShiftInRegister();
   setupShiftOutRegister();
+
+#ifdef ROTATE_BOARD
+  Serial.print("Will rotate board by ");
+  Serial.print(ROTATE_BOARD);
+  Serial.println(" degrees");
+#else
+  Serial.print("Will rotate board by 0 degrees");
+#endif
+#ifdef FLIP_BOARD
+  Serial.println("Will flip board");
+#else
+  Serial.println("Will not flip board");
+#endif
 
   Wire.begin(I2C_ADDRESS);
   Serial.print("Joining I2C bus at address 0x");
@@ -80,8 +103,48 @@ uint64_t scanBoard() {
     temp |= colsDown;
   }
   writeShiftOutRegister(0);
+#if ROTATE_BOARD == 90
+  temp = rotateBoard90Degrees(temp);
+#elif ROTATE_BOARD == 180
+  temp = rotateBoard90Degrees(temp);
+  temp = rotateBoard90Degrees(temp);
+#elif ROTATE_BOARD == 270
+  temp = rotateBoard90Degrees(temp);
+  temp = rotateBoard90Degrees(temp);
+  temp = rotateBoard90Degrees(temp);
+#endif
   return temp;
 }
+
+#ifdef ROTATE_BOARD
+uint64_t rotateBoard90Degrees(uint64_t board) {
+  const uint8_t n = 8;
+  // https://stackoverflow.com/a/35137982/10291933
+  for (int i = 0; i < n; i += 1) {
+    for (int j = i + 1; j < n; j += 1) {
+      // swap(data[i][j], data[j][i]);
+      const uint8_t ijPos = i * 8 + j;
+      const uint8_t jiPos = j * 8 + i;
+      const bool ij = bitRead(board, ijPos);
+      const bool ji = bitRead(board, jiPos);
+      bitWrite64(board, jiPos, ij);
+      bitWrite64(board, ijPos, ji);
+    }
+  }
+  for (int i = 0; i < n; i += 1) {
+    for (int j = 0; j < n / 2; j += 1) {
+      // swap(data[i][j], data[i][n - 1 - j]);
+      const uint8_t ijPos = i * 8 + j;
+      const uint8_t in1jPos = i * 8 + (n - 1 - j);
+      const bool ij = bitRead(board, ijPos);
+      const bool in1j = bitRead(board, in1jPos);
+      bitWrite64(board, in1jPos, ij);
+      bitWrite64(board, ijPos, in1j);
+    }
+  }
+  return board;
+}
+#endif
 
 void printBoard() {
   for (uint8_t i = 0; i < 64; i++) {
@@ -111,7 +174,13 @@ uint8_t readShiftInRegister() {
 
   digitalWrite(shiftInClockPin, HIGH);
   digitalWrite(shiftInClockEnablePin, LOW);
-  uint8_t data = ~shiftIn(shiftInDataInPin, shiftInClockPin, MSBFIRST);
+  uint8_t data = ~shiftIn(shiftInDataInPin, shiftInClockPin,
+#ifdef FLIP_BOARD
+                          LSBFIRST
+#else
+                          MSBFIRST
+#endif
+  );
   digitalWrite(shiftInClockEnablePin, HIGH);
 
   return data;
@@ -129,7 +198,7 @@ void setupShiftOutRegister() {
 
 void writeShiftOutRegister(uint8_t data) {
   digitalWrite(shiftOutLatchPin, LOW);
-  shiftOut(shiftOutDataPin, shiftOutClockPin, MSBFIRST, data);
+  shiftOut(shiftOutDataPin, shiftOutClockPin, LSBFIRST, data);
   digitalWrite(shiftOutLatchPin, HIGH);
 }
 
